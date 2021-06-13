b837371588bc3c5e811550feddd13532f74dc290
Alessandro Corra <alessandro.corra@lovehoney.co.uk>
2021-06-13 16:34:12 +0100

    enzyme added

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { mount } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('portfolio App component', () => {
+  it('renders', () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.find('.app').exists()).toBe(true);
+  });
 });
