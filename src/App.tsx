d8672208ac09cbdd71d41f4671e19f9746199968
Alessandro Corra <alessandro.corra@lovehoney.co.uk>
2021-06-13 14:57:15 +0100

    prettier and eslint set

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          My react-ts portfolio is under construction
-        </p>
+        <p>My react-ts portfolio is under construction</p>
         <a
           className="App-link"
           href="https://github.com/alecorra/portfolio"
