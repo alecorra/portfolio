9024c702b485264948ce6061fa70b6b298121d8a
Alessandro Corra <alessandro.corra@lovehoney.co.uk>
2021-06-13 16:08:16 +0100

    boilerplate removed, sass added

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.scss';
+
+export const App: React.FunctionComponent = () => {
+  return (
+    <div className="app">
+      <h1 className="app__title">PORTFOLIO</h1>
+    </div>
+  );
+};
+
+export default App;
